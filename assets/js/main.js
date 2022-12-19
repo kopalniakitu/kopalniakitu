e27cfa1b25d4218f192a54567af6693f33824b5c
kopalniakitu <116721842+kopalniakitu@users.noreply.github.com>
2022-12-19 14:46:51 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-//
+// Cookie notice
+const cookieNotice = document.querySelector(".cookie-notice");
+const cookieNoticeButton = cookieNotice.querySelector(".button");
+
+if (!localStorage.getItem("cookies-accpted")) {
+  cookieNotice.classList.remove("-hidden");
+}
+
+cookieNoticeButton.addEventListener("click", () => {
+  cookieNotice.classList.add("-hidden");
+  localStorage.setItem("cookies-accpted", "1");
+});
